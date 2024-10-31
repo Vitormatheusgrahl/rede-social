58c2fba73874fd1d8a08e17764a4a50adad645f9
Vitor <108533713+Vitormatheusgrahl@users.noreply.github.com>
2024-10-31 11:46:38 -0300

    Add files via upload

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,3 +1,4 @@
+import { criarGrafico } from "./common"
 
 
 
@@ -62,10 +63,7 @@ async function quantidadeUsuariosPorRede() {
 
 
 
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, layout)
+criarGrafico(data, layout)
 }
 
 
